c9f09e253b35f930cbbc2dda279afb88143fc5f3
Mohammed Dawood <mahmedtony73@gmail.com>
2021-10-19 12:13:51 +0200

    adding fetch function and implementing apis add comments and upvotes

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router";
+import AddCommentForm from "../components/AddCommentForm";
 import ArticleList from "../components/ArticleList";
+import CommentsList from "../components/CommentsList";
+import UpvotesSection from "../components/UpvotesSection";
 import articleContent from "./article-content";
 import NotFoundPage from "./NotFoundPage";
 
 function ArticlePage() {
   const params = useParams();
+
   const article = articleContent.find(
     (article) => article.name === params.name
   );
+
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(`/api/articles/${params.name}`);
+      const body = await result.json();
+      setArticleInfo(body);
+    };
+    fetchData();
+  }, [params.name]);
+
   if (!article) {
     return <NotFoundPage />;
   }
@@ -18,9 +33,20 @@ function ArticlePage() {
   return (
     <>
       <h1>{article.title}</h1>
+      <UpvotesSection
+        articleName={params.name}
+        upvotes={articleInfo.upvotes}
+        setArticleInfo={setArticleInfo}
+      />
       {article.content.map((paragraph, key) => (
         <p key={key}>{paragraph}</p>
       ))}
+
+      <CommentsList comments={articleInfo.comments} />
+      <AddCommentForm
+        articleName={params.name}
+        setArticleInfo={setArticleInfo}
+      />
       <h3>Other Articles:</h3>
       <ArticleList articles={otherArticles} />
     </>
